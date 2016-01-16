9cf5cfb118c72ffc8c03fddda487eaf01e17beda
Aaron <pioneervon@gmail.com>
2016-01-16 21:18:23 +0800

    Add timer
    Signed-off-by: Aaron <pioneervon@gmail.com>

--- dismd5.sh
+++ dismd5.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+start=$(date "+%s")
 IFS=$(echo -en "\n\b")
 path=$1
-path=${path// /\\s}
-# echo $path
+# path=${path// /\\s}
+echo $path
 files=`ls $1`
 
 echo "0" > x
@@ -12,11 +13,13 @@ for f in $files;
 do
   #random name
   rand=$(head -200 /dev/urandom | cksum | cut -f1 -d" ")
-  name=$path"dismd5/"${rand}".dmg"
+  name=$path"/dismd5/"${rand}".dmg"
   # f=${f// /\\ }
-  f=$path$f
+  f=$path"/"$f
   # echo $f
   # echo $name
   cat $f ./x > $name
-
 done
+
+now=$(date "+%s")
+echo "Time elapsed: $((now-start)) secs"
