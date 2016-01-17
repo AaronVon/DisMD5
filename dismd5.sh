51ccedd1d3938fd64c85f80d0aedf4d3b6573b1b
Aaron <pioneervon@gmail.com>
2016-01-17 10:16:13 +0800

    Roughly I thought there is no need to multi thread, as I processed 7 files of 12.4GB in volume which turn out that single thread is much faster than multi thread
    Signed-off-by: Aaron <pioneervon@gmail.com>

--- dismd5.sh
+++ dismd5.sh
@@ -3,22 +3,23 @@ start=$(date "+%s")
 IFS=$(echo -en "\n\b")
 path=$1
 # path=${path// /\\s}
-echo $path
+# echo $path
 files=`ls $1`
 
 echo "0" > x
 mkdir $path"/dismd5"
 
 for f in $files;
-do
+do {
   #random name
   rand=$(head -200 /dev/urandom | cksum | cut -f1 -d" ")
   name=$path"/dismd5/"${rand}".dmg"
   # f=${f// /\\ }
   f=$path"/"$f
-  # echo $f
+  echo $f
   # echo $name
   cat $f ./x > $name
+}
 done
 
 now=$(date "+%s")
